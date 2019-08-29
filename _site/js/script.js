48c3929934ad95059bc23dc83055f9b6ead96faf
emmadrews <drews.emma@gmail.com>
2019-08-29 13:41:14 -0500

    added content

--- _site/js/script.js
+++ _site/js/script.js
@@ -87,35 +87,6 @@ $(document).ready(function() {
 
   })( window, document );
 
-//owl carousel
-
-
-	  var owl = $("#owl-carousel");
-
-	  owl.owlCarousel({
-	      items : 2, //10 items above 1000px browser width
-	      itemsDesktop : [1000,1], //5 items between 1000px and 901px
-	      itemsDesktopSmall : [900,1], // betweem 900px and 601px
-	      itemsTablet: [600,1], //2 items between 600 and 0
-	      itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-				autoPlay: true,
-				stopOnHover: true,
-				navigationText: true
-	  });
-
-	  // Custom Navigation Events
-	  $(".next").click(function(){
-	    owl.trigger('owl.next');
-	  })
-	  $(".prev").click(function(){
-	    owl.trigger('owl.prev');
-	  })
-	  $(".play").click(function(){
-	    owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-	  })
-	  $(".stop").click(function(){
-	    owl.trigger('owl.stop');
-	  })
 
 //toggle trancated testimonials to expand
 $('.item-toggle').on('click', function(event){
